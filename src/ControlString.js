2e10d85d283170886cce5fedd92528258c39fda4
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-11-26 04:56:15 +0000

    Update ControlString.js add slider

--- src/ControlString.js
+++ src/ControlString.js
@@ -1,4 +1,4 @@
-import { Slider } from "@mui/material";
+// import { Slider } from "@mui/material";
 import { range } from "lodash";
 import React from "react";
 // import { Slider } from "@mui/material";
@@ -17,13 +17,14 @@ export default function ControlString({
     <select value={metric} onChange={(e) => selectMetric(e.target.value)}>
       <option value="from">to</option>
       <option value="to">from</option>
-      <option value="between">between</option>
+      <option value="between">net to</option>
     </select>
   );
 
   const yearSelect = (
-    <>
-    <select value={year} onChange={(e) => selectYear(e.target.value)}>
+    <div style={{ position: 'relative', display: 'inline-block', fontFamily: 'monospace', margin: "0 15px" }}>
+      20{year}
+    {/* <select value={year} onChange={(e) => selectYear(e.target.value)}>
       {range(5, 20)
         .map((n) => String(n).padStart(2, 0))
         .map((y) => (
@@ -31,8 +32,12 @@ export default function ControlString({
             20{y}
           </option>
         ))}
-    </select>
-      <Slider
+    </select> */}
+    <input onChange={(e) => selectYear(e.target.value.padStart(2, 0))} 
+    style={{ position: 'absolute', top: 16, left: "-50%", width: "200%" }} type="range" min="05" max="19" value={year} />
+    </div>
+  );
+      {/* <Slider
   aria-label="Temperature"
   defaultValue={2000+year}
   // getAriaValueText={valuetext}
@@ -41,9 +46,7 @@ export default function ControlString({
   marks
   min={2005}
   max={2019}
-/>
-    </>
-  );
+/> */}
 
   const weightedSet = (
     <>
@@ -53,16 +56,20 @@ export default function ControlString({
         id="weighted"
         name="weighted"
         value="weighted"
+        style={{marginLeft: 14}}
         onChange={() => setWeighted(!weighted)}
       />
-      <label for="weighted">weighted</label>(pp 100,000)
+      <label for="weighted">weighted</label>
+      {/* <caption>(per 100,000)</caption> */}
     </>
   );
 
   return (
-    <div>
+    <div style={{ marginTop: 24 }}>
       How many people moved {metricSelect} {selectedState}{" "}
-      {metric === "between" ? "net" : metric} in {yearSelect}? {weightedSet}
+      {/* {metric === "between" ? "net" : metric} */}
+      in {yearSelect}
+      {weightedSet}
       {/* <Slider /> */}
 {/* <Slider defaultValue={30} step={10} marks min={10} max={110} disabled /> */}
     </div>
