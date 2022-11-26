e99a8f32bfaae205d7ad5e8c1daf6901ec218664
Josh Maas-Howard <j.maas.howard@gmail.com>
2022-11-25 22:56:54 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+import MapChart from "./MapChart";
+import ControlString from "./ControlString";
+
+function App() {
+  const [selectedState, setSelecetedState] = React.useState("AK");
+  const [year, selectYear] = React.useState("19");
+  const [metric, selectMetric] = React.useState("between");
+  const [weighted, setWeighted] = React.useState(true);
+  console.log(selectedState);
+  return (
+    <div>
+      {/* viewing data for {selectedState} */}
+      <ControlString
+        selectedState={selectedState}
+        selectYear={selectYear}
+        year={year}
+        selectMetric={selectMetric}
+        metric={metric}
+        weighted={weighted}
+        setWeighted={setWeighted}
+      />
+      <MapChart
+        selectedState={selectedState}
+        selectHandler={setSelecetedState}
+        year={year}
+        metric={metric}
+        weighted={weighted}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
